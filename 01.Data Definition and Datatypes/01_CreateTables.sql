6d9b901f04e4739a211779e56825c9af3d709d79
Georgi Tangardzhiev <98902611+GeorgiKyshenko@users.noreply.github.com>
2022-08-22 20:57:21 +0300

    Add files via upload

---
+++ 01.Data Definition and Datatypes/01_CreateTables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employees (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(50),
+last_name VARCHAR(50)
+);
+CREATE TABLE categories (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL
+);
+CREATE TABLE products (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(50) NOT NULL,
+category_id INT,
+CONSTRAINT cat_fk
+FOREIGN KEY (category_id)
+REFERENCES gamebar.categories(id)
+);
